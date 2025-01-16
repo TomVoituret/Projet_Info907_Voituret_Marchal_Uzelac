381faf4c39e8798af57bddb0b5f349aea5b8ca48
TOOm <97885350+TomVoituret@users.noreply.github.com>
2025-01-16 17:14:18 +0100

    Truvc stylé quoiu

--- public/script.js
+++ public/script.js
@@ -15,21 +15,72 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
-    // Fonction pour afficher les données
     const renderTable = (data) => {
         tableBody.innerHTML = ''; // Vider le tableau avant d'afficher les nouvelles données
         data.forEach(arme => {
             const row = document.createElement('tr');
             row.innerHTML = `
+                <td><img src="${arme.image_url}" alt="${arme.modele}" width="100" height="auto"></td>
                 <td>${arme.modele}</td>
-                <td>${arme.categorie ? Object.keys(arme.categorie).find(cat => arme.categorie[cat] === 1) : ''}</td>
-                <td>${arme.mode['un-coup']['a 1 coup'] ? '1 coup' : 'Multi-coup'}</td>
-                <td>${arme.canon['canon-lisse'] ? 'Canon lisse' : 'Canon rayé'}</td>
-                <td>${arme.utilisation.cible_reelle.cible_legales.chasse ? 'Chasse' : 'Autre'}</td>
+                <td>${formatCategorie(arme.categorie)}</td>
+                <td>${formatMode(arme.mode)}</td>
+                <td>${formatProjectile(arme.projectile)}</td>
+                <td>${formatUtilisation(arme.utilisation)}</td>
             `;
             tableBody.appendChild(row);
         });
     };
+    
+    // Fonction pour formater la catégorie
+    function formatCategorie(categorie) {
+        // Retourne la clé de la catégorie où la valeur est 1
+        return Object.keys(categorie).find(cat => categorie[cat] === 1) || 'Non spécifié';
+    }
+    
+    // Fonction pour formater le mode de tir
+    function formatMode(mode) {
+        // Recherche du mode "un-coup"
+        const unCoupMode = Object.keys(mode['un-coup']).filter(key => mode['un-coup'][key] === 1);
+        // Recherche du mode "multi-coup"
+        const multiCoupMode = Object.keys(mode['multi-coup']).filter(key => mode['multi-coup'][key] === 1);
+        
+        // Si on trouve des modes "un-coup", les afficher
+        if (unCoupMode.length > 0) {
+            return `${unCoupMode.join(', ')}`;
+        }
+        
+        // Si on trouve des modes "multi-coup", les afficher
+        if (multiCoupMode.length > 0) {
+            return `${multiCoupMode.join(', ')}`;
+        }
+        
+        return 'Mode non spécifié';
+    }
+    
+    // Fonction pour formater le type de canon
+    function formatProjectile(projectile) {
+        const projectileType = Object.keys(projectile).filter(key => projectile[key] === 1);
+        if (projectileType.length > 0) {
+            return projectileType.join(', ');
+        }
+        return 'Type de projectile non spécifié';
+    }
+    
+    // Fonction pour formater l'utilisation de l'arme
+    function formatUtilisation(utilisation) {
+        const legalTargets = Object.keys(utilisation.cible_reelle.cible_legales).filter(key => utilisation.cible_reelle.cible_legales[key] === 1);
+        const illegalTargets = Object.keys(utilisation.cible_reelle.cible_illegales).filter(key => utilisation.cible_reelle.cible_illegales[key] === 1);
+        const notReelTargets = Object.keys(utilisation.cible_theorique).filter(key => utilisation.cible_theorique[key] === 1);
+
+        const allTargets = [...legalTargets, ...illegalTargets, ...notReelTargets];
+        
+        if (allTargets.length > 0) {
+            return allTargets.join(', ');
+        }
+        
+        return 'Utilisation non spécifiée';
+    }
+
 
     // Fonction pour filtrer les données
     const filterData = (data, query) => {
@@ -61,6 +112,51 @@ document.getElementById('searchBox').addEventListener('keypress', (event) => {
     }
 });
 
+    // Fonction pour récupérer toutes les valeurs sélectionnées
+    const getSelectedValues = () => {
+    const selectedValues = {
+      porte: [],
+      prise: [],
+      mecanisme: []
+    };
+    
+    // Récupérer les valeurs sélectionnées pour "Porte"
+    document.querySelectorAll('input[id^="porte_"]:checked').forEach(checkbox => {
+      selectedValues.porte.push(checkbox.value);
+    });
+
+    // Récupérer les valeurs sélectionnées pour "Prise"
+    document.querySelectorAll('input[id^="prise_"]:checked').forEach(checkbox => {
+      selectedValues.prise.push(checkbox.value);
+    });
+
+    // Récupérer les valeurs sélectionnées pour "Mécanisme"
+    document.querySelectorAll('input[id^="mecanisme_"]:checked').forEach(checkbox => {
+      selectedValues.mecanisme.push(checkbox.value);
+    });
+
+    return selectedValues;
+  };
+
+  // Fonction pour filtrer les armes selon les valeurs sélectionnées
+  const filterWeapons = () => {
+    const selectedValues = getSelectedValues();
+    console.log(selectedValues)
+    const filteredData = dataGlobal.filter(arme => {
+      const porteMatch = selectedValues.porte.length === 0 || selectedValues.porte.some(value => arme.porte[value] === 1);
+      const priseMatch = selectedValues.prise.length === 0 || selectedValues.prise.some(value => arme.prise[value] === 1);
+      const mecanismeMatch = selectedValues.mecanisme.length === 0 || selectedValues.mecanisme.some(value => arme.mecanisme[value] === 1) || selectedValues.mecanisme.some(value => arme.mecanisme.automatique[value] === 1);
+      return porteMatch && priseMatch && mecanismeMatch;
+    });
+    renderTable(filteredData);
+  };
+
+  // Appeler filterWeapons chaque fois qu'une case est cochée ou décochée
+  document.querySelectorAll('.form-check-input').forEach(input => {
+    input.addEventListener('change', filterWeapons);
+  });
+
     // Chargement initial des données
     fetchData().then(data => renderTable(data));
+
 });
